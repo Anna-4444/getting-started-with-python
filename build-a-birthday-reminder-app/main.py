efea886ff6fb09d619a2312315afad3f5aabaef3
anna_b <nelson_anna9@hotmail.com>
2025-08-28 13:43:41 -0500

    Add feature to search for birthday

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,9 @@
 user_input = input("Which birthday are you looking for? ")
+if user_input == "Nate":
+    print("Birthday is 02/23/82!")
+elif user_input == "Lucy":
+    print("Birthday is 01/07/10!")
+elif user_input == "Sam":
+    print("Birthday is 07/22/12!")
+else:
+    print("Can't find a birthday for this person...")
