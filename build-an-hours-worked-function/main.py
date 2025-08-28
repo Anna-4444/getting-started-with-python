17f7872324c4a94181de00b622d9361573ca6971
anna_b <nelson_anna9@hotmail.com>
2025-08-28 15:20:56 -0500

    Add calculate monthly pay function

--- build-an-hours-worked-function/main.py
+++ build-an-hours-worked-function/main.py
@@ -7,9 +7,19 @@ def calculate_pay(hours_worked, pay_per_hour):
         return regular_pay + overtime_pay
     else:
         return hours_worked * pay_per_hour
+
+def calculate_monthly_pay(week1_hours, week2_hours, week3_hours, week4_hours, pay_per_hour):
+    week1_pay = calculate_pay(week1_hours, pay_per_hour)
+    week2_pay = calculate_pay(week2_hours, pay_per_hour)
+    week3_pay = calculate_pay(week3_hours, pay_per_hour)
+    week4_pay = calculate_pay(week4_hours, pay_per_hour)
+    return week1_pay + week2_pay + week3_pay + week4_pay 
+
 # Worked 40 hours at $20 an hour
 print(calculate_pay(40, 20))
 # Worked 50 hours at $20 an hour
 print(calculate_pay(50, 20))
 # Worked 40 hours at $12 an hour
 print(calculate_pay(40, 12))
+
+print(calculate_monthly_pay(40,50,35,40,50))
