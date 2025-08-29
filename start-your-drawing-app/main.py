761cfd538e05e243b6d4a4f2f54f9465b9e192f3
anna_b <nelson_anna9@hotmail.com>
2025-08-29 11:16:01 -0500

    Draw a red hexagon

---
+++ start-your-drawing-app/main.py
@@ -0,0 +1,15 @@
+import turtle as turtle;
+
+turtle.pencolor("red")
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
