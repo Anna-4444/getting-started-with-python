cb474c7695c2121490d8b5c4ea397ed9f686861e
anna_b <nelson_anna9@hotmail.com>
2025-08-29 14:53:00 -0500

    Add to do list featrue

--- build-a-to-do-list/main.py
+++ build-a-to-do-list/main.py
@@ -3,3 +3,11 @@ done = False
 
 print("Type 'done' at any time to exit")
 
+while not done:
+    new_item = input("Add item to list: ")
+    if new_item.lower() == "done":
+        done = True
+    else:
+        todo_list.append(new_item) 
+
+print(f"Here is your to-do-list {todo_list}")   
