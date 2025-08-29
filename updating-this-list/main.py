6dd74e9bf2bd0890e2a7a720c73f977e37b5e815
anna_b <nelson_anna9@hotmail.com>
2025-08-29 13:49:53 -0500

    Create list of friends

---
+++ updating-this-list/main.py
@@ -0,0 +1,12 @@
+friends = ["Lucy", "Nate", "Sam", "Margaret", "Lizzy", "Ellen", "Shelly", "Katie", "Julie", "Amy", "Kara", "Candi", "Jenny", "Jacki", "Stephanie", "Rachael", "Anette", "Lindsay", "Veronica", "Valerie"]
+dinner_friends = friends[0:4]
+print(dinner_friends)
+
+drink_friends = friends[len(friends) - 4:]
+print(drink_friends)
+
+hike_friends = friends[10:14]
+print(hike_friends)
+
+walk_friends = friends[5:6]
+print(walk_friends)
