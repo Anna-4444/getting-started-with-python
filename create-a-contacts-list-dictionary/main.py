00a04a477c012b69eedcda7cc4acd754a236b36b
anna_b <nelson_anna9@hotmail.com>
2025-09-02 11:44:43 -0500

    Create a personal assistant class

--- create-a-contacts-list-dictionary/main.py
+++ create-a-contacts-list-dictionary/main.py
@@ -5,9 +5,26 @@ company_org = {}
 for i in range(len(people)):
     person = people[i]
     person_title = title[i]
+
+
     company_org[person] = person_title
 
-print(company_org)
+# print(company_org)
 
+
+class PersonalAssistant:
+    def __init__(self):
+        self.contacts = company_org
+        self.todos = []
+
+    def get_contact(self, name):
+        if name in self.contacts:
+            return self.contacts[name]
+        else:
+            return "No contact with that name"
+
+
+assistant = PersonalAssistant()
+print(assistant.get_contact("Nichole"))
 
 
