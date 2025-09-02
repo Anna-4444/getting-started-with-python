d5b082fbdf05a3cd304aff77bedb86b84082918a
anna_b <nelson_anna9@hotmail.com>
2025-09-02 13:34:50 -0500

    Read txt file into json file

--- convert-file-types/main.py
+++ convert-file-types/main.py
@@ -1,2 +1,9 @@
 import json
 show_data = []
+
+with open("television_shows.txt", "r") as file:
+    for line in file:
+        show_data.append(line)
+
+with open("best_shows.json", "w") as file:
+    json.dump( show_data, file)
