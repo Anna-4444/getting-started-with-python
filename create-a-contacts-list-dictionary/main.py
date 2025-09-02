510d9c7d31a5d80c988f1248b7836a1191ded7f3
anna_b <nelson_anna9@hotmail.com>
2025-09-02 12:21:25 -0500

    Add personal assistant methods

--- create-a-contacts-list-dictionary/main.py
+++ create-a-contacts-list-dictionary/main.py
@@ -17,6 +17,33 @@ class PersonalAssistant:
         self.contacts = company_org
         self.todos = []
 
+    def add_todo(self, new_item):
+        self.todos.append(new_item)
+
+    def remove_todo(self, todo_item):
+        if todo_item in self.todos:
+            index = self.todos.index(todo_item)
+            self.todos.pop(index)
+        else:
+            print("Todo is not in list!")
+        # for i in range(len(self.todos)):
+        #     if todo_item == self.todos[i]:
+        #        return self.todos.pop(i)        
+        # print("Todo is not in list!")
+
+    def get_todos(self):
+        return self.todos
+
+    def get_birthday(self, name):
+        if name == "Nate":
+            print("Birthday is 02/23/82!")
+        elif name == "Lucy":
+            print("Birthday is 01/07/10!")
+        elif name == "Sam":
+            print("Birthday is 07/22/12!")
+        else:
+            print("Can't find a birthday for this person...")
+
     def get_contact(self, name):
         if name in self.contacts:
             return self.contacts[name]
@@ -26,5 +53,11 @@ class PersonalAssistant:
 
 assistant = PersonalAssistant()
 print(assistant.get_contact("Nichole"))
+assistant.add_todo("Groceries")
+assistant.add_todo("Clean")
+print(assistant.get_todos())
+assistant.remove_todo("Clean")
+print(assistant.get_todos())
+print(assistant.get_birthday("Nate"))
 
 
